8209b0501afc26ecb143679176321a7ff37e1f02
jeielaraneta <jeielaraneta@gmail.com>
2019-03-03 12:51:17 +0800

    initialize basic structure

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # dw-dashboard
 
 So I can pass my subject :)
+
+1. Enrollment - number of enrollment *bar chart
+	- School year
+		- semester
+			- College
+				- courses
+2. Employees - number of employees *pie chart
+	- Non-teaching
+		- Department
+			- Employee category (Permanent, Casual, J.o) 
+	- Teaching
+		- Department
+			- Employee category (Permanent, Casual, COS)
+				- Academic Rank
+3. Budget Allocation - budget *bar chart
+	- School Year
+		- Department
+4. Research and Extension - total ng target of researches *bar graph
+	- College
--- dashboard.js
+++ dashboard.js
@@ -4,7 +4,7 @@ Highcharts.chart('container', {
         type: 'column'
     },
     title: {
-        text: 'Basic drilldown'
+        text: 'Enrollment Population'
     },
     xAxis: {
         type: 'category'
@@ -24,39 +24,116 @@ Highcharts.chart('container', {
     },
 
     series: [{
-        name: 'Things',
+        name: 'Population of Enrollees',
         colorByPoint: true,
         data: [{
-            name: 'Animals',
-            y: 5,
-            drilldown: 'animals'
+            name: '2017-2018',
+            y: 450,
+            drilldown: '201718'
         }, {
-            name: 'Fruits',
-            y: 2,
-            drilldown: 'fruits'
+            name: '2018-2019',
+            y: 20,
+            drilldown: '201819'
         }, {
-            name: 'Cars',
-            y: 4,
-            drilldown: 'cars'
+            name: '2019-2020',
+            y: 0,
+            drilldown: ' '
         }]
     }],
     drilldown: {
             series: [{
-                id: 'animals',
-                name: 'Animals',
+                id: '201718',
+                name: 'Population',
                 data: [{
-                    name: 'Cats',
-                    y: 4,
-                    drilldown: 'cats'
-                }, ['Dogs', 2],
-                    ['Cows', 1],
-                    ['Sheep', 2],
-                    ['Pigs', 1]
+                        name: 'Summer',
+                        y: 200,
+                        drilldown: 'summer2018'
+                    }, {
+                        name: '1st Semester',
+                        y: 100,
+                        drilldown: 'firstSem2018'
+                    }, {
+                        name: '2nd Semester',
+                        y: 150,
+                        drilldown: 'secondSem2018'
+                    }
                 ]
-            }, {
 
-                id: 'cats',
-                data: [1, 2, 3]
+            }, {
+                id: 'summer2018',
+                data: [{
+                    name: 'Engineering',
+                    y: 30
+                }, {
+                    name: 'Computer Studies',
+                    y: 44
+                }, {
+                    name: 'Business and Public Administration',
+                    y: 35
+                }, {
+                    name: 'Education',
+                    y: 25
+                }, {
+                    name: 'Arts and Sciences',
+                    y: 65
+                }]
+            }, {
+                id: 'firstSem2018',
+                data: [{
+                    name: 'Engineering',
+                    y: 10
+                }, {
+                    name: 'Computer Studies',
+                    y: 20
+                }, {
+                    name: 'Business and Public Administration',
+                    y: 30
+                }, {
+                    name: 'Education',
+                    y: 25
+                }, {
+                    name: 'Arts and Sciences',
+                    y: 15
+                }]
+            }, {
+                id: 'secondSem2018',
+                data: [{
+                    name: 'Engineering',
+                    y: 10
+                }, {
+                    name: 'Computer Studies',
+                    y: 30
+                }, {
+                    name: 'Business and Public Administration',
+                    y: 50
+                }, {
+                    name: 'Education',
+                    y: 10
+                }, {
+                    name: 'Arts and Sciences',
+                    y: 50
+                }]
+            }, {
+                id: '201819',
+                name: 'Population',
+                data: [{
+                        name: 'Summer',
+                        y: 5,
+                        drilldown: 'summer2019'
+                    }, {
+                        name: '1st Semester',
+                        y: 15,
+                        drilldown: 'summer'
+                    }, {
+                        name: '2nd Semester',
+                        y: 5,
+                        drilldown: 'summer'
+                    }
+                ]
+
+            }, {
+                id: 'summer2019',
+                data: [1,2,3]
             }]
         }
 });
